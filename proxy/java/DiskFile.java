8a6f76e1c415b0c8e5802fbd0309baaab534401e
Pedro Felipe de Azevedo Furtado <pedrofurtado@users.noreply.github.com>
2018-03-31 18:29:57 -0300

    Create DiskFile.java

---
+++ proxy/java/DiskFile.java
@@ -0,0 +1,18 @@
+public class DiskFile extends File {
+    public DiskFile(String name) {
+        this.name = name;
+        this.loadFromDisk();
+    }
+    
+    public void display() {
+        System.out.format("Rendering of file '%s' on screen...\n", this.name);
+        // sleep
+        System.out.format("'%s' rendered.\n", this.name);
+    }
+    
+    private void loadFromDisk() {
+        System.out.format("Loading from disk the file '%s' ...\n", this.name);
+        // colocar sleep
+        System.out.format("'%s' loaded from disk.\n", this.name);
+    }
+}
