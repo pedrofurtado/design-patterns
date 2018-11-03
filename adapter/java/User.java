2c08712d67f625620884f7a210019bee67078cb0
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2018-11-02 21:12:32 -0300

    Update User.java

--- adapter/java/User.java
+++ adapter/java/User.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class User {
+public class User {
     public String username;
     public String password;
     public Map<String, String> additionalAttributes = new HashMap<String, String>();
