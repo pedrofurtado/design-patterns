d0104d9a8cc682325142deea34bb566636636ef1
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2018-11-02 21:11:52 -0300

    Create User.java

---
+++ adapter/java/User.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class User {
+    public String username;
+    public String password;
+    public Map<String, String> additionalAttributes = new HashMap<String, String>();
+}
