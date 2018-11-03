1071e9ddba03b1ba604284e7dae085c65760617b
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2018-11-02 21:14:39 -0300

    Update Client.java

--- adapter/java/Client.java
+++ adapter/java/Client.java
@@ -1,21 +1,20 @@
 public class Client {
-  public static void main(String[] args) {
-    RectangleAdapter rectangleAdapter = new RectangleAdapter();
+    public static void main(String args[]) {
+        SocialLoginAdapter adapter = new GooglePlusAdapter(); // or FacebookAdapter or TwitterAdapter
 
-    int x1 = 2;
-    int y1 = 4;
+        SocialAuthenticator socialAuthenticator = new SocialAuthenticator(adapter, "clientId", "clientSecret", "redirectUrl");
 
-    int x4 = 5;
-    int y4 = 1;
+        System.out.format("%nAuthentication result: %s .%n", socialAuthenticator.auth("another-username", "another-password") ? "Success" : "Error");
 
-    rectangleAdapter.draw(x1, y1, x4, y4);
+        User fetchedUserByCredentials = socialAuthenticator.fetchUserFromCredentials("some-username", "some-password");
+        System.out.format("%nUser fetched with username '%s' and password '%s'.%n", fetchedUserByCredentials.username, fetchedUserByCredentials.password);
 
-    SquareAdapter squareAdapter = new SquareAdapter();
+        User fetchedUserByToken = socialAuthenticator.fetchUserFromToken("my-token");
+        System.out.format("%nUser fetched with username '%s' and password '%s'.%n", fetchedUserByToken.username, fetchedUserByToken.password);
 
-    x1 = 5;
-    y1 = 0;
-    int size = 9;
-
-    squareAdapter.draw(x1, y1, size);
-  }
+        if (fetchedUserByToken.additionalAttributes.containsKey("tweetsAmount"))    { System.out.format("%nAdditional attributes tweetsAmount: %s.%n", fetchedUserByToken.additionalAttributes.get("tweetsAmount")); }
+        if (fetchedUserByToken.additionalAttributes.containsKey("followersAmount")) { System.out.format("%nAdditional attributes followersAmount: %s.%n", fetchedUserByToken.additionalAttributes.get("followersAmount")); }
+        if (fetchedUserByToken.additionalAttributes.containsKey("friendsAmount"))   { System.out.format("%nAdditional attributes friendsAmount: %s.%n", fetchedUserByToken.additionalAttributes.get("friendsAmount")); }
+        if (fetchedUserByToken.additionalAttributes.containsKey("groupsAmount"))    { System.out.format("%nAdditional attributes groupsAmount: %s.%n", fetchedUserByToken.additionalAttributes.get("groupsAmount")); }
+    }
 }
